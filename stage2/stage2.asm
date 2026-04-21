59b244e5d3ace97d0cade9df4973ebf070e19e2a
Magnus R. Souza <magnus.rs@hotmail.com>
2026-04-20 23:17:32 -0300

    correções stage2

--- stage2/stage2.asm
+++ stage2/stage2.asm
@@ -2,18 +2,66 @@ BITS 16
 ORG 0x8000
 
 start:
-    mov si, msg
+    cli
+    xor ax, ax
+    mov ds, ax
+    sti
 
-print:
-    lodsb
-    cmp al, 0
-    je hang
+    mov ax, 0x8000
+    mov es, ax
+    mov di, 0x1000       ; buffer longe do código
 
-    mov ah, 0x0E
-    int 0x10
-    jmp print
+    xor ebx, ebx
 
-hang:
+.loop:
+    mov eax, 0xE820
+    mov edx, 0x534D4150  ; "SMAP"
+    mov ecx, 20
+
+    int 0x15
+    jc .done
+
+    cmp eax, 0x534D4150
+    jne .done
+
+    ; imprime tipo
+    mov ax, [es:di + 16]
+    call print_hex
+
+    mov al, 13
+    call putc
+    mov al, 10
+    call putc
+
+    add di, 20
+
+    cmp ebx, 0
+    jne .loop
+
+.done:
     jmp $
 
-msg db 'Stage 2 carregado com sucesso!', 0
+; --------
+putc:
+    mov ah, 0x0E
+    int 0x10
+    ret
+
+print_hex:
+    mov cx, 4
+.hex:
+    rol ax, 4
+    mov dl, al
+    and dl, 0x0F
+
+    cmp dl, 9
+    jbe .num
+    add dl, 7
+.num:
+    add dl, '0'
+
+    mov al, dl
+    call putc
+
+    loop .hex
+    ret
